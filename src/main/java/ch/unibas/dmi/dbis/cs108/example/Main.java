6009ae9283a74027460264409f198729eff33ced
Vincent Schall <v.schall@stud.unibas.ch>
2025-03-05 11:47:40 +0100

    added files HexTile, HexMap and Main (all .java). creating a baseline for the gamedesign as a part of milestone 1 requirements.

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/Main.java
@@ -0,0 +1,11 @@
+package ch.unibas.dmi.dbis.cs108.example;
+
+/**
+ * testing the HexMap and HexTiles classes
+ */
+public class Main {
+    public static void main(String[] args){
+        HexMap map = new HexMap(3, 3);
+        map.printMap();
+    }
+}
