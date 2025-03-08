74fb732632a300d7892e053d6789470b496a7e97
Noah Valentin Klaholz <noahk2006@gmx.de>
2025-03-08 08:33:09 +0100

    Added gradle task for creating java docs according to comments in methods and classes. Started work on client/server API interface

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/server/CommunicationAPI.java
@@ -0,0 +1,18 @@
+package ch.unibas.dmi.dbis.cs108.server;
+
+/**
+ * Interface for the communication between the server and the client
+ */
+public interface CommunicationAPI {
+    /**
+     * Sends a message to the server
+     * @param message the message to send
+     */
+    void sendMessage(String message);
+
+    /**
+     * Receives a message from the server
+     * @return the message received
+     */
+    String receiveMessage();
+}
