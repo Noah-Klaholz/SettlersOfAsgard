e2c34081519e5127fedb9f90f98a957d3d4e91e2
Vincent Schall <v.schall@stud.unibas.ch>
2025-03-15 12:31:20 +0100

    Added all remaining network commands to the CommunicationAPI.NetworkProtocol.

--- src/main/java/ch/unibas/dmi/dbis/cs108/server/CommunicationAPI.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/server/CommunicationAPI.java
@@ -20,10 +20,34 @@ public interface CommunicationAPI {
      * Utility class for network protocol constants
      */
     class NetworkProtocol {
+        // administrative commands
         public final static String TEST = "TEST"; // Test command
         public final static String SHUTDOWN = "STDN"; // Shutdown command broadcast by server to disconnect all clients
+        public final static String JOIN = "JOIN"; // Player joins a game
+        public final static String EXIT = "EXIT"; // Player exits a game
+        public final static String CHATGLOBAL = "CHTG"; // Send a message to all players
+        public final static String CHATPRIVATE = "CHTP"; // Send a whisper message to only one player
+        public final static String LISTLOBBIES = "LIST"; // List all current Lobbies
+        public final static String START = "STRT"; // Start the game
+        public final static String STATS = "STAT"; // Request game state
+        public final static String SYNCHRONIZE = "SYNC"; // Request synchronization of the game
+        // game mechanics
+        public final static String STARTTURN = "TURN"; // starts turn
+        public final static String ENDTURN = "ENDT"; // ends turn
+        public final static String BUYHEXFIELD = "BUYH"; // player buys a hexfield
+        public final static String BUILDSTRUCTURE = "BILD"; // player builds a structure
+        public final static String UPGRADESTRUCTURE = "UPGD"; // player upgrades a structure
+        public final static String TRADERESOURCES = "TRAD"; // player offers a trade of resources to another player
+        public final static String RESOURCEBALANCE = "BLNC"; // request the current resource balance of a player
+        public final static String STARTRITUAL = "RITU"; // player starts a ritual
+        public final static String BLESSING = "BLES"; // player gets blessed
+        public final static String CURSE = "CURS"; // player gets cursed
+        public final static String USEARTIFACT = "ARTF"; // player uses an artifact
+        public final static String FINDARTIFACT = "FIND"; // player finds an artifact
+        // exception handling
         public final static String OK = "OK"; // OK response
         public final static String ERROR = "ERR"; // Error response
+
     }
 }
 
