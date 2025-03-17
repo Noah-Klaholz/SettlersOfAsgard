4ec7b5770b1a20a90136d8c1d6f28a8e6ac2b708
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 12:57:48 +0100

    feat(protocol): add MessageParser class structure

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageParser.java
@@ -0,0 +1,11 @@
+package ch.unibas.dmi.dbis.cs108.client.networking.protocol;
+
+public class MessageParser {
+    public static void parse(String message) {
+
+    }
+
+    private static void handleChatMessage(String message) {
+
+    }
+}
