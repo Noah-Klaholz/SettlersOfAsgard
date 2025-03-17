e8d3275de937caa45ed2d6d65083f4bc75332761
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 10:20:11 +0100

    feat(networking): add CommandFactory class structure

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/CommandFactory.java
@@ -0,0 +1,7 @@
+package ch.unibas.dmi.dbis.cs108.client.commands;
+
+public class CommandFactory {
+    public static GameCommand createCommand(String type, String data) {
+        return null;
+    }
+}
