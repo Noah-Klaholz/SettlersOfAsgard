b7387e9605a88f21f07b8de4ea800599f3979944
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 13:12:25 +0100

    feat(protocol): implement parse in MessageParser

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageParser.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageParser.java
@@ -2,7 +2,13 @@ package ch.unibas.dmi.dbis.cs108.client.networking.protocol;
 
 public class MessageParser {
     public static void parse(String message) {
-
+        if (message.startsWith("CHAT:")) {
+            handleChatMessage(message);
+        } else if (message.startsWith("UPDATE_BOARD")) {
+            System.out.println("[Parser] Received board update: " + message);
+        } else {
+            System.out.println("[Parser] Unrecognized message: " + message);
+        }
     }
 
     private static void handleChatMessage(String message) {
