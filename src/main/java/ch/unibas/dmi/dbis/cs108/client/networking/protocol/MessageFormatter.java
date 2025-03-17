b6089dd2ffe6f02d1fcce483c7a65f2d2d88c593
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 12:47:36 +0100

    feat(protocol): add MessageFormatter class structure

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageFormatter.java
@@ -0,0 +1,7 @@
+package ch.unibas.dmi.dbis.cs108.client.networking.protocol;
+
+public class MessageFormatter {
+    public static String formatChatMessage(String username, String message) {
+        return null;
+    }
+}
