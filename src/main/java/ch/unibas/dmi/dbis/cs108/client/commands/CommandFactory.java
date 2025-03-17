599b73fea3cb0377ccbc733b54d9828fc1c16373
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 10:32:12 +0100

    docs(GameClient): add Javadoc to CommandFactory class

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/CommandFactory.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/CommandFactory.java
@@ -1,6 +1,15 @@
 package ch.unibas.dmi.dbis.cs108.client.commands;
 
+/**
+ * CommandFactory class is responsible for creating commands
+ */
 public class CommandFactory {
+    /**
+     * Creates a command
+     * @param type Type of command
+     * @param data Data of command
+     * @return GameCommand
+     */
     public static GameCommand createCommand(String type, String data) {
         return null;
     }
