162a927a4a1b312811e26738290f816dac0bf14c
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 09:17:31 +0100

    feat(networking): add GameClient class skeleton

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/GameClient.java
@@ -0,0 +1,40 @@
+package ch.unibas.dmi.dbis.cs108.client.networking;
+
+import ch.unibas.dmi.dbis.cs108.client.observer.GameEventListener;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class GameClient {
+    private static GameClient instance; // Singleton instance
+    private Socket socket;
+    private BufferedReader input;
+    private PrintWriter output;
+    private boolean connected;
+    private GameEventListener listener;
+
+    private GameClient(String serverAddress, int port) {
+
+    }
+
+    public static GameClient getInstance(String serverAddress, int port) {
+        return null;
+    }
+
+    public void setListener(GameEventListener listener) {
+
+    }
+
+    public void sendMessage(String message) {
+
+    }
+
+    private void listenForMessages() {
+
+    }
+
+    public void disconnect() {
+
+    }
+}
