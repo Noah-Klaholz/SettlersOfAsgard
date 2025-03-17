d9918bd745c9967e762e92a08c30392d196ded2b
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 10:29:59 +0100

    docs(GameClient): add Javadoc to CommandSender class

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/CommandSender.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/CommandSender.java
@@ -3,13 +3,25 @@ package ch.unibas.dmi.dbis.cs108.client.networking;
 import ch.unibas.dmi.dbis.cs108.client.commands.CommandFactory;
 import ch.unibas.dmi.dbis.cs108.client.commands.GameCommand;
 
+/**
+ * CommandSender class is responsible for sending commands to the server
+ */
 public class CommandSender {
     private GameClient client;
 
+    /**
+     * Constructor for CommandSender
+     * @param client GameClient
+     */
     public CommandSender(GameClient client) {
         this.client = client;
     }
 
+    /**
+     * Sends a command to the server
+     * @param type Type of command
+     * @param data Data of command
+     */
     public void sendCommand(String type, String data) {
         GameCommand command = CommandFactory.createCommand(type, data);
         client.sendMessage(command.execute());
