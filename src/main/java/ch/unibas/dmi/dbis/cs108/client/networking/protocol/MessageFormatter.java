fd597e9cfb2af9e66f9a40ee7a0532256676e4ac
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 13:20:24 +0100

    docs(MessageFormatter): add Javadoc comments for methods

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageFormatter.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageFormatter.java
@@ -1,6 +1,15 @@
 package ch.unibas.dmi.dbis.cs108.client.networking.protocol;
 
+/**
+ * This class provides static methods to format messages according to the protocol.
+ */
 public class MessageFormatter {
+    /**
+     * Formats a chat message.
+     * @param username The username of the sender.
+     * @param message The message to be sent.
+     * @return The formatted message.
+     */
     public static String formatChatMessage(String username, String message) {
         return "CHAT:" + username + ":" + message;
     }
