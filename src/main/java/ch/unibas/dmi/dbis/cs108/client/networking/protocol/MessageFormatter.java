9e44f33c5e2c1fc4bccaecd5db9245055e77e2bf
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-17 13:07:43 +0100

    feat(protocol): implement formatChatMessage in MessageFormatter

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageFormatter.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/protocol/MessageFormatter.java
@@ -2,6 +2,6 @@ package ch.unibas.dmi.dbis.cs108.client.networking.protocol;
 
 public class MessageFormatter {
     public static String formatChatMessage(String username, String message) {
-        return null;
+        return "CHAT:" + username + ":" + message;
     }
 }
