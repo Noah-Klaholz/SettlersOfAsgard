31e56c5c77e508a51c868dc6bf8e06429bd68cf2
JanuraRose <j.staisch@stud.unibas.ch>
2025-03-18 17:50:34 +0100

    feat(commands): add UseArtifactCommand skeleton

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/UseArtifactCommand.java
@@ -0,0 +1,17 @@
+package ch.unibas.dmi.dbis.cs108.client.commands;
+
+import ch.unibas.dmi.dbis.cs108.server.Command;
+
+public class UseArtifactCommand implements GameCommand {
+
+    private String data;
+
+    public UseArtifactCommand(String data) {
+
+    }
+
+    @Override
+    public String execute() {
+        return null;
+    }
+}
