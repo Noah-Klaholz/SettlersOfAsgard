82b54dc82eb7d5f35ea816cf72d799dca1586f83
JanuraRose <j.staisch@stud.unibas.ch>
2025-03-18 17:38:46 +0100

    feat(commands): add BuyTileCommand

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/BuyTileCommand.java
@@ -0,0 +1,15 @@
+package ch.unibas.dmi.dbis.cs108.client.commands;
+
+
+public class BuyTileCommand implements GameCommand {
+
+    private String data;
+
+    public BuyTileCommand(String data) {
+
+    }
+    @Override
+    public String execute() {
+        return null;
+    }
+}
