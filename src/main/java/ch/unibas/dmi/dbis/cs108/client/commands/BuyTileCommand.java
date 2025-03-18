5b290cb96ea89f67f44afb78781dc8e20be440a4
JanuraRose <j.staisch@stud.unibas.ch>
2025-03-18 17:41:54 +0100

    feat(commands): add documentation in BuyTileCommand

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/BuyTileCommand.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/BuyTileCommand.java
@@ -5,10 +5,18 @@ public class BuyTileCommand implements GameCommand {
 
     private String data;
 
+    /**
+     * Constructor for the BuyTileCommand
+     * @param data the data for the command
+     */
     public BuyTileCommand(String data) {
         this.data = data;
     }
 
+    /**
+     * Executes the command
+     * @return the command as a String
+     */
     @Override
     public String execute() {
         return "BUY_TILE:" + data;
