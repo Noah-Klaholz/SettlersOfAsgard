630b77a1ddea50207f32f7ed812c76849f6c3ccf
JanuraRose <j.staisch@stud.unibas.ch>
2025-03-18 17:53:10 +0100

    feat(commands): add documentation in UseArtifactCommand

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/UseArtifactCommand.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/commands/UseArtifactCommand.java
@@ -4,10 +4,18 @@ public class UseArtifactCommand implements GameCommand {
 
     private String data;
 
+    /**
+     * Constructor for the UseArtifactCommand
+     * @param data the data for the command
+     */
     public UseArtifactCommand(String data) {
         this.data = data;
     }
 
+    /**
+     * Executes the command
+     * @return the command as a String
+     */
     @Override
     public String execute() {
         return "USE_ARTIFACT:" + data;
