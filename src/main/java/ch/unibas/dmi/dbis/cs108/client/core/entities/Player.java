7cf3e94864a6a04dcf6033e55fe612203c6e7ba3
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-20 13:42:05 +0100

    feat(entities): add getters and setters for Player properties

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Player.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Player.java
@@ -6,4 +6,16 @@ public class Player {
 
     public Player(String id, String name) {
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
