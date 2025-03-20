c7ef90edf7afaf46ee9f337cf1b519673d19c4fa
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-20 13:46:39 +0100

    docs(entities): add Javadoc comments to Player class

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Player.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Player.java
@@ -1,22 +1,42 @@
 package ch.unibas.dmi.dbis.cs108.client.core.entities;
 
+/**
+ * Player class is responsible for creating a player object
+ */
 public class Player {
     private final String id;
     private String name;
 
+    /**
+     * Constructor for Player class
+     * @param id String
+     * @param name String
+     */
     public Player(String id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Getter for id
+     * @return String
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Getter for name
+     * @return String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for name
+     * @param name String
+     */
     public void setName(String name) {
         this.name = name;
     }
