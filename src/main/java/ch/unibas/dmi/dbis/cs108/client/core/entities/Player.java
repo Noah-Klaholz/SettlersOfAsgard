581d397b77b63cb6119da964ce87e6a75d7f2f18
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-20 13:38:31 +0100

    feat(entities): add Player class structure

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Player.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Player.java
@@ -1,4 +1,9 @@
 package ch.unibas.dmi.dbis.cs108.client.core.entities;
 
 public class Player {
+    private String id;
+    private String name;
+
+    public Player(String id, String name) {
+    }
 }
