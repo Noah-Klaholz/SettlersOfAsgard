d3a0451320fb27661753d927c4138baa3d4b846a
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-03-21 20:37:04 +0100

    feat(commands): add PingCommand class

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/commands/PingCommand.java
@@ -0,0 +1,19 @@
+package ch.unibas.dmi.dbis.cs108.client.core.commands;
+
+import ch.unibas.dmi.dbis.cs108.client.core.entities.Player;
+
+public class PingCommand implements Command {
+    private final Player sender;
+
+    public PingCommand(Player sender) {
+        this.sender = sender;
+    }
+
+    public Player getSender() {
+        return sender;
+    }
+
+    public void execute() {
+        System.out.println("Local: " + sender.getName() + " is pinging the server");
+    }
+}
