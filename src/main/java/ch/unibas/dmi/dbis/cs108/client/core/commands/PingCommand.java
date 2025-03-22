e5e861568ce8cfb2cc12ace9748f6cb2c6ba59a8
Noah Valentin Klaholz <noahk2006@gmx.de>
2025-03-22 11:45:19 +0100

    doc(PingCommand): Added documentation to class

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/commands/PingCommand.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/commands/PingCommand.java
@@ -2,17 +2,33 @@ package ch.unibas.dmi.dbis.cs108.client.core.commands;
 
 import ch.unibas.dmi.dbis.cs108.client.core.entities.Player;
 
+/**
+ * PingCommand class is responsible for creating a ping command
+ * This command is used to check the connection to the server
+ */
 public class PingCommand implements Command {
     private final Player sender;
 
+    /**
+     * Constructor for the PingCommand
+     * @param sender
+     */
     public PingCommand(Player sender) {
         this.sender = sender;
     }
 
+    /**
+     * Getter for sender
+     * @return Player who sent the command
+     */
     public Player getSender() {
         return sender;
     }
 
+    /**
+     * Executes the command
+     * Prints playerName who is pinging the server
+     */
     @Override
     public void execute() {
         System.out.println("Local: " + sender.getName() + " is pinging the server");
