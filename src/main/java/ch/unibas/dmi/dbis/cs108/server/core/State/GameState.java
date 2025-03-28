560cd3290e092fc40f48a9c6af72f51f1571ef36
Noah Valentin Klaholz <Noah.Klaholz@stud.unibas.ch>
2025-03-28 11:44:20 +0100

    feat(GameState): Added GameState class and docs (no functionality).

--- src/main/java/ch/unibas/dmi/dbis/cs108/server/core/State/GameState.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/server/core/State/GameState.java
@@ -1,13 +1,172 @@
 package ch.unibas.dmi.dbis.cs108.server.core.State;
 
+/**
+ * The GameState class is responsible for storing and managing the current state
+ * of a game session. It tracks information such as:
+ * <ul>
+ *     <li>The player whose turn it is</li>
+ *     <li>The current game round (1-5)</li>
+ *     <li>The game field represented as a two-dimensional array</li>
+ *     <li>List of players in the game</li>
+ *     <li>Each player's artifacts and rune count</li>
+ * </ul>
+ * This class provides getter and setter methods for all game state properties.
+ */
 public class GameState {
 
     /**
-     * Klasse sollte speichern:
-     * - Spieler, der am Zug ist
-     * - Game-Runde (1-5)
-     * - zweidimnsionales Array für das Spielfeld
-     * - Array: Liste der Spieler
-     * - für jeden Spieler: Liste welche Artefakte er hat, wie viel Runen besitzt er
+     * Gets the name of the player whose turn it currently is.
+     *
+     * @return The name of the player whose turn it is, or null if not set
      */
+    public String getPlayerTurn() {
+        return null;
+    }
+
+    /**
+     * Sets the player whose turn it currently is.
+     *
+     * @param player The name of the player whose turn it is
+     */
+    public void setPlayerTurn(String player) {
+    }
+
+    /**
+     * Gets the current game round.
+     *
+     * @return The current game round as a string, or null if not set
+     */
+    public String getGameRound() {
+        return null;
+    }
+
+    /**
+     * Sets the current game round.
+     *
+     * @param round The game round number (1-5)
+     */
+    public void setGameRound(int round) {
+    }
+
+    /**
+     * Gets the list of players in the game.
+     *
+     * @return A string representation of the players, or null if not set
+     */
+    public String getPlayers() {
+        return null;
+    }
+
+    /**
+     * Sets the list of players in the game.
+     *
+     * @param players An array of player names
+     */
+    public void setPlayers(String[] players) {
+    }
+
+    /**
+     * Gets the artifacts owned by a specific player.
+     *
+     * @param player The name of the player
+     * @return An array of artifacts owned by the player, or null if not set
+     */
+    public String[] getArtifacts(String player) {
+        return null;
+    }
+
+    /**
+     * Sets the artifacts for a specific player.
+     *
+     * @param artifacts An array of artifacts to assign to the player
+     * @param player The name of the player
+     */
+    public void setArtifacts(String[] artifacts, String player) {
+    }
+
+    /**
+     * Adds artifacts to a player's collection.
+     *
+     * @param artifacts The artifacts to add
+     * @param player The name of the player
+     */
+    public void addArtifact(String[] artifacts, String player) {}
+
+    /**
+     * Removes artifacts from a player's collection.
+     *
+     * @param artifacts The artifacts to remove
+     * @param player The name of the player
+     */
+    public void removeArtifact(String[] artifacts, String player) {}
+
+    /**
+     * Gets the number of runes a player has.
+     *
+     * @param player The name of the player
+     * @return The number of runes the player has
+     */
+    public int getRunes(String player) {
+        return 0;
+    }
+
+    /**
+     * Sets the number of runes for a player.
+     *
+     * @param runes The number of runes
+     * @param player The name of the player
+     */
+    public void setRunes(int runes, String player) {
+    }
+
+    /**
+     * Adds runes to a player's collection.
+     *
+     * @param runes The number of runes to add
+     * @param player The name of the player
+     */
+    public void addRunes(int runes, String player) {}
+
+    /**
+     * Removes runes from a player's collection.
+     * @param runes The number of runes to remove
+     * @param player The name of the player
+     */
+    public void removeRunes(String[] runes, String player) {}
+
+    /**
+     * Gets the entire game field.
+     *
+     * @return A two-dimensional array representing the game field, or null if not set
+     */
+    public int[][] getGameField() {
+        return null;
+    }
+
+    /**
+     * Sets the entire game field.
+     *
+     * @param gameField A two-dimensional array representing the game field
+     */
+    public void setGameField(int[][] gameField) {}
+
+    /**
+     * Gets the value at a specific position on the game field.
+     *
+     * @param x The x-coordinate on the game field
+     * @param y The y-coordinate on the game field
+     * @return The value at the specified position
+     */
+    public int getGameField(int x, int y) {
+        return 0;
+    }
+
+    /**
+     * Sets the value at a specific position on the game field.
+     *
+     * @param x The x-coordinate on the game field
+     * @param y The y-coordinate on the game field
+     * @param value The value to set at the specified position
+     */
+    public void setGameField(int x, int y, int value) {}
 }
