a0283264045430505ef03952831673fa07d222dd
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-04 19:46:03 +0200

    feat(artefact): add FenrirsBones as a concrete artefact

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/artefacts/FenrirsBones.java
@@ -0,0 +1,7 @@
+package ch.unibas.dmi.dbis.cs108.client.core.entities.artefacts;
+
+public class FenrirsBones extends Artefact {
+    public FenrirsBones(ArtefactData data) {
+        super(data);
+    }
+}
