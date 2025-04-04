7ba6ba813231ffd03926a9138f87410a6aba1977
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-04 19:41:42 +0200

    feat(artefact): add TearOfYggdrasil as a concrete artefact

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/artefacts/TearOfYggdrasil.java
@@ -0,0 +1,7 @@
+package ch.unibas.dmi.dbis.cs108.client.core.entities.artefacts;
+
+public class TearOfYggdrasil extends Artefact {
+    public TearOfYggdrasil(ArtefactData data) {
+        super(data);
+    }
+}
