1865b954fc9447e9a75a43ff300edc679e8d9bfb
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-04 19:45:50 +0200

    feat(artefact): add FenrirsChains as a concrete artefact

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/artefacts/FenrirsChains.java
@@ -0,0 +1,7 @@
+package ch.unibas.dmi.dbis.cs108.client.core.entities.artefacts;
+
+public class FenrirsChains extends Artefact {
+    public FenrirsChains(ArtefactData data) {
+        super(data);
+    }
+}
