87d38229f4e19cdc6eba604f84d5917e0260fe8c
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-04 19:49:00 +0200

    refactor(core): remove setters from StructureData for immutability

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/structures/StructureData.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/structures/StructureData.java
@@ -8,54 +8,27 @@ public class StructureData {
     private String useType;
     private int cost;
 
-    public StructureData() {
-    }
-
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getUsage() {
         return usage;
-    }
-
-    public void setUsage(String usage) {
-        this.usage = usage;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getUseType() {
         return useType;
     }
 
-    public void setUseType(String useType) {
-        this.useType = useType;
-    }
-
     public int getCost() {
         return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
     }
 }
