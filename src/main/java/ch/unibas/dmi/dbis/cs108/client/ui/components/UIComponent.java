2d2fab0e843a626270b2b2f51dca80caad6f8571
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-04 15:59:43 +0200

    feat(ui): add abstract UIComponent base class for reusable JavaFX components

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/components/UIComponent.java
@@ -0,0 +1,33 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.components;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Base class for reusable UI components.
+ *
+ * @param <T> The type of JavaFX Node encapsulated by the component.
+ */
+public abstract class UIComponent<T extends Node> {
+    private static final Logger logger = Logger.getLogger(UIComponent.class.getName());
+    protected T view;
+
+    public UIComponent(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            loader.setController(this);
+            this.view = loader.load();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to load component from: " + fxmlPath, e);
+            throw new RuntimeException("Failed to load component from: " + fxmlPath, e);
+        }
+    }
+
+    public T getView() {
+        return view;
+    }
+}
