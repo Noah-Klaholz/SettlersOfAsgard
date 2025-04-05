efa41a6d41a8632a77cfc8fd1fe35d561c171d0a
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-05 19:02:01 +0200

    feat(events): add StatueActivatedEvent

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/events/StatueActivatedEvent.java
@@ -0,0 +1,30 @@
+package ch.unibas.dmi.dbis.cs108.client.core.events;
+
+import ch.unibas.dmi.dbis.cs108.client.core.entities.Player;
+import ch.unibas.dmi.dbis.cs108.client.core.entities.Statue;
+
+public class StatueActivatedEvent implements GameEvent {
+    public enum Outcome { DEAL, BLESSING, CURSE }
+
+    private final Player player;
+    private final Statue statue;
+    private final Outcome outcome;
+
+    public StatueActivatedEvent(Player player, Statue statue, Outcome outcome) {
+        this.player = player;
+        this.statue = statue;
+        this.outcome = outcome;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Statue getStatue() {
+        return statue;
+    }
+
+    public Outcome getOutcome() {
+        return outcome;
+    }
+}
