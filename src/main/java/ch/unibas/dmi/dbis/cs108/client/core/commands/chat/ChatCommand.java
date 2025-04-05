48c14641e99accd0a0249e50b39d589d6ea1c9a7
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-05 17:18:29 +0200

    refactor(chat): uncomment ChatCommand.java

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/commands/chat/ChatCommand.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/commands/chat/ChatCommand.java
@@ -1,53 +1,53 @@
-package ch.unibas.dmi.dbis.cs108.client.core.commands.chat;
-
-import ch.unibas.dmi.dbis.cs108.client.core.commands.Command;
-import ch.unibas.dmi.dbis.cs108.client.core.entities.Player;
-
-/**
- * ChatCommand class is responsible for creating a chat command
- * This command is used to send a message to the chat
- */
-public class ChatCommand implements Command {
-    private final Player sender;
-    private final String message;
-
-    /**
-     * Constructor for ChatCommand class
-     *
-     * @param sender  Player
-     * @param message String
-     */
-    public ChatCommand(Player sender, String message) {
-        this.sender = sender;
-        this.message = message;
-    }
-
-    /**
-     * Getter for message
-     *
-     * @return String
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * Getter for sender
-     *
-     * @return Player
-     */
-    public Player getSender() {
-        return sender;
-    }
-
-    /**
-     * Executes the command
-     */
-    @Override
-    public void execute() {
-        // Only handle local effects like:
-        // - Updating the local chat UI
-        // - Adding to chat history
-        System.out.println("Local: " + sender.getName() + ": " + message);
-    }
-}
+//package ch.unibas.dmi.dbis.cs108.client.core.commands.chat;
+//
+//import ch.unibas.dmi.dbis.cs108.client.core.commands.Command;
+//import ch.unibas.dmi.dbis.cs108.client.core.entities.Player;
+//
+///**
+// * ChatCommand class is responsible for creating a chat command
+// * This command is used to send a message to the chat
+// */
+//public class ChatCommand implements Command {
+//    private final Player sender;
+//    private final String message;
+//
+//    /**
+//     * Constructor for ChatCommand class
+//     *
+//     * @param sender  Player
+//     * @param message String
+//     */
+//    public ChatCommand(Player sender, String message) {
+//        this.sender = sender;
+//        this.message = message;
+//    }
+//
+//    /**
+//     * Getter for message
+//     *
+//     * @return String
+//     */
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    /**
+//     * Getter for sender
+//     *
+//     * @return Player
+//     */
+//    public Player getSender() {
+//        return sender;
+//    }
+//
+//    /**
+//     * Executes the command
+//     */
+//    @Override
+//    public void execute() {
+//        // Only handle local effects like:
+//        // - Updating the local chat UI
+//        // - Adding to chat history
+//        System.out.println("Local: " + sender.getName() + ": " + message);
+//    }
+//}
