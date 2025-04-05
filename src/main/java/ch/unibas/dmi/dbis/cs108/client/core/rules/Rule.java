d6ccf8877a7b22ecd3d15422b28c343e0348b522
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-05 17:23:31 +0200

    feat(rules): add Rule

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/rules/Rule.java
@@ -0,0 +1,8 @@
+package ch.unibas.dmi.dbis.cs108.client.core.rules;
+
+import ch.unibas.dmi.dbis.cs108.client.core.state.GameState;
+
+interface Rule {
+    boolean isApplicable(GameState gameState);
+    void apply(GameState gameState);
+}
