8338542b490b1d96fb56253c8e92d5bf422d848d
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-05 17:17:11 +0200

    feat(entities): add Board

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Board.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/entities/Board.java
@@ -1,7 +1,35 @@
 package ch.unibas.dmi.dbis.cs108.client.core.entities;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.List;
 
-    //todo: implement board class
-    //besitzt alle set Tiles (auch Tiles mit FixedStructures)
+public class Board {
+    private List<Tile> tiles;
+
+    public Board() {
+        tiles = new ArrayList<>();
+    }
+
+    //todo: 2d array statt list
+
+    // Initialize board with a specified number of fields.
+    public void initBoard(int numberOfTiles) {
+//        for (int i = 0; i < numberOfTiles; i++) {
+//            // For demo purposes, resourceValue is simply (i+1)
+//            tiles.add(new Tile(i, i + 1));
+//        }
+    }
+
+    public Tile getTile(int id) {
+        for (Tile tile : tiles) {
+            if (tile.getTileID() == id) {
+                return tile;
+            }
+        }
+        return null;
+    }
+
+    public List<Tile> getTiles() {
+        return tiles;
+    }
 }
