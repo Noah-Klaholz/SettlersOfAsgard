b478fa17e4ecfd52d97ae95ae6ef2bb33c7aa7b6
Noah Valentin Klaholz <Noah.Klaholz@stud.unibas.ch>
2025-04-06 16:50:24 +0200

    feat(ReceiveCommandEvent): Finished implementation, used CommunicationAPI (shared) for CommandTypes. Note: Not all CommandTypes have to be supported! Some are Server only.

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/ReceiveCommandEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/ReceiveCommandEvent.java
@@ -1,63 +1,27 @@
 package ch.unibas.dmi.dbis.cs108.client.ui.events;
 
+import ch.unibas.dmi.dbis.cs108.shared.protocol.CommunicationAPI.NetworkProtocol.*;
+
+/**
+ * Event representing a command received from the server.
+ * Uses the shared CommunicationAPI to define the command types.
+ */
 public class ReceiveCommandEvent {
     private final String message;
-    private final CommandType commandType;
+    private final Commands commandType;
 
+    // Commands have OK added in front of them, but this is removed upon event creation
     public ReceiveCommandEvent(String message) {
         this.message = message.replaceAll("OK\\$", "").trim().toLowerCase();
-        this.commandType = CommandType.fromCommand(message.split(" ")[0]);
+        this.commandType = Commands.fromCommand(message.split("\\$")[0]);
     }
 
     public String getMessage() {
         return message;
     }
 
-    public CommandType getType() {
+    public Commands getType() {
         return commandType;
-    }
-
-    public enum CommandType {
-        // All / Commands (except ping, because user should not be able to send it)
-        EXIT("/exit"),
-        CHANGENAME("/changename"),
-        JOINLOBBY("/join"),
-        LEAVELOBBY("/leave"),
-        CREATELOBBY("/create"),
-        STARTGAME("/start"),
-        LISTLOBBIES("/listlobbies"),
-        LISTALLPLAYERS("/listallplayers"),
-        LISTLOBBYPLAYERS("/listlobbyplayers"),
-        GLOBALCHAT("/global"),
-        HELP("/help"),
-        BUYTILE("/buytile"),
-        PLACESTRUCTURE("/placestructure"),
-        USESTRUCTURE("/usestructure"),
-        UPGRADESTATUE("/upgradestructure"),
-        USESTATUE("/usestatue"),
-        USEFIELDARTIFACT("/usefieldartifact"),
-        USEPLAYERARTIFACT("/useplayerartifact"),
-        STATUS("/status"),
-        PRICES("/prices");
-
-        private final String command;
-
-        CommandType(String command) {
-            this.command = command;
-        }
-
-        public String getCommand() {
-            return command;
-        }
-
-        public static CommandType fromCommand(String command) {
-            for (CommandType type : values()) {
-                if (type.command.equalsIgnoreCase(command)) {
-                    return type;
-                }
-            }
-            throw new IllegalArgumentException("Unknown SendCommand-Event command: " + command);
-        }
     }
 
 }
