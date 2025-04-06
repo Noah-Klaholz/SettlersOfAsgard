7a49cc345f48d844a025e9b3cbda705890ff13a1
Noah Valentin Klaholz <Noah.Klaholz@stud.unibas.ch>
2025-04-06 06:42:22 +0200

    feat(Events): Implemented Statue and Structure PurchasedEvent classes.

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/events/StructurePurchasedEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/events/StructurePurchasedEvent.java
@@ -1,4 +1,29 @@
 package ch.unibas.dmi.dbis.cs108.client.core.events;
 
-public class StructurePurchasedEvent {
+import ch.unibas.dmi.dbis.cs108.client.core.entities.Player;
+import ch.unibas.dmi.dbis.cs108.client.core.entities.Tile;
+import ch.unibas.dmi.dbis.cs108.client.core.entities.Structure;
+
+public class StructurePurchasedEvent implements GameEvent {
+    private final Player player;
+    private final Tile tile;
+    private final Structure structure;
+
+    public StructurePurchasedEvent(Player player, Tile tile, Structure structure) {
+        this.player = player;
+        this.tile = tile;
+        this.structure = structure;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public Structure getStructure() {
+        return structure;
+    }
 }
