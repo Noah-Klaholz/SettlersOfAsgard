97a3d2d332b624ddf38e64f3a6e0147715c7ad8c
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-14 09:12:31 +0200

    feat(events): add PlayerJoinedEvent class with placeholder method

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/PlayerJoinedEvent.java
@@ -0,0 +1,8 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events;
+
+public class PlayerJoinedEvent {
+    //ToDo: Implement the PlayerJoinedEvent class
+    public String getPlayerName() {
+        return "Placeholder for player name";
+    }
+}
