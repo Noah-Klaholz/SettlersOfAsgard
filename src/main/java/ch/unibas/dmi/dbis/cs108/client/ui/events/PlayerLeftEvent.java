3f8ad22a4e5d15fb19ee81b475ef371c78804a9f
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-14 09:12:12 +0200

    feat: add event classes and update GameApplication initialization

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/PlayerLeftEvent.java
@@ -0,0 +1,8 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events;
+
+public class PlayerLeftEvent {
+    // ToDo: Implement the PlayerLeftEvent class
+    public String getPlayerName() {
+        return "Placeholder for player name";
+    }
+}
