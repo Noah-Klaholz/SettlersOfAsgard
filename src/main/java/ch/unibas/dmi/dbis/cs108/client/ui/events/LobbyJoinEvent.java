3e834886404adfb76c4e92f6c0e65fc9d454aee7
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-14 09:13:00 +0200

    feat(events): add placeholder for LobbyJoinEvent class

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/LobbyJoinEvent.java
@@ -0,0 +1,8 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events;
+
+public class LobbyJoinEvent {
+    // ToDo: Implement the LobbyJoinEvent class
+    public String getLobbyData() {
+        return "Placeholder for lobby data";
+    }
+}
