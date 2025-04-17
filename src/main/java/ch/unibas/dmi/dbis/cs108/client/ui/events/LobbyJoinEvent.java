75aa93fc349a5e0b35b03364f7de6f26aaa15f40
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-17 15:16:50 +0200

    refactor: commands in ui

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/LobbyJoinEvent.java
+++
@@ -1,8 +0,0 @@
-package ch.unibas.dmi.dbis.cs108.client.ui.events;
-
-public class LobbyJoinEvent {
-    // ToDo: Implement the LobbyJoinEvent class
-    public String getLobbyData() {
-        return "Placeholder for lobby data";
-    }
-}
--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/PlayerLeftEvent.java
+++
@@ -1,8 +0,0 @@
-package ch.unibas.dmi.dbis.cs108.client.ui.events;
-
-public class PlayerLeftEvent {
-    // ToDo: Implement the PlayerLeftEvent class
-    public String getPlayerName() {
-        return "Placeholder for player name";
-    }
-}
