f1a9521bedb3a0554ae3f834a4202ce8d7e086dc
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-17 15:30:03 +0200

    feat(game): add UseFieldArtifactUIEvent for handling artifact usage

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/UseFieldArtifactUIEvent.java
@@ -0,0 +1,38 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
+
+import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
+
+public class UseFieldArtifactUIEvent implements UIEvent {
+    private final int x;
+    private final int y;
+    private final int artifactId;
+    private final String useType;
+
+    public UseFieldArtifactUIEvent(int x, int y, int artifactId, String useType) {
+        this.x = x;
+        this.y = y;
+        this.artifactId = artifactId;
+        this.useType = useType;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getArtifactId() {
+        return artifactId;
+    }
+
+    public String getUseType() {
+        return useType;
+    }
+
+    @Override
+    public String getType() {
+        return "USEFIELDARTIFACT";
+    }
+}
