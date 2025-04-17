b0eb21ff525cf163a20319893542cc2690319cc4
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-17 15:28:12 +0200

    feat: add BuyTileUIEvent class for handling tile purchase events

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/BuyTileUIEvent.java
@@ -0,0 +1,26 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
+
+import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
+
+public class BuyTileUIEvent implements UIEvent {
+    private final int x;
+    private final int y;
+
+    public BuyTileUIEvent(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String getType() {
+        return "BUYTILE";
+    }
+}
