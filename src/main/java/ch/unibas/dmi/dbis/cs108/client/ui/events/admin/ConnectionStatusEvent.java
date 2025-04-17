c47eff06a18ae73f1e16e927e252453ccecddb26
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-17 15:17:49 +0200

    feat: add ConnectionStatusEvent to represent client connection status

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/ConnectionStatusEvent.java
@@ -0,0 +1,41 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events.admin;
+
+
+import ch.unibas.dmi.dbis.cs108.client.networking.events.ConnectionEvent.ConnectionState;
+/**
+ * UIEvent representing the connection status of the client.
+ */
+public class ConnectionStatusEvent {
+    private final ConnectionState status;
+    private final String message;
+
+    /**
+     * Constructor for ConnectionStatusEvent.
+     *
+     * @param status  The status of the connection.
+     * @param message The message associated with the connection status.
+     */
+    public ConnectionStatusEvent(ConnectionState status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    /**
+     * Get the status of the connection.
+     *
+     * @return The connection status.
+     */
+    public ConnectionState getStatus() {
+        return status;
+    }
+
+    /**
+     * Get the message associated with the connection status.
+     *
+     * @return The message.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+}
