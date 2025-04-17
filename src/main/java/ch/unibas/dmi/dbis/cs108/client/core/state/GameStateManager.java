ec0db0c7375f2ebdcb3ee7f66be09c8edb7aed8f
Vincent Schall <v.schall@stud.unibas.ch>
2025-04-17 12:36:54 +0200

    feat(client): added javadoc to GameStateManager

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/state/GameStateManager.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/state/GameStateManager.java
@@ -2,24 +2,50 @@ package ch.unibas.dmi.dbis.cs108.client.core.state;
 
 import java.util.logging.Logger;
 
+/**
+ * This class is responsible for managing a gameState.
+ * It will update the gameState based on a message from the server.
+ */
 public class GameStateManager {
 
+    /** The gameState object managed by this class */
     private GameState gameState;
+    /** Logger to log logging*/
     private static final Logger LOGGER = Logger.getLogger(GameStateManager.class.getName());
 
+    /**
+     * Creates a new object of this class
+     *
+     * @param gameState the gameState to manage
+     */
     GameStateManager(GameState gameState) {
         this.gameState = gameState;
     }
 
+    /**
+     * Gets the current gameState
+     * @return the gameState
+     */
     public GameState getGameState() {
         return gameState;
     }
 
+    /**
+     * Sets the current gameState
+     *
+     * @param gameState the gameState to set
+     */
     public void setGameState(GameState gameState) {
         this.gameState = gameState;
     }
 
+    /**
+     * Updates the gameState by parsing the message from the server.
+     *
+     * @param message the message from the server.
+     * @see ch.unibas.dmi.dbis.cs108.server.core.model.GameStateSerializer
+     */
     public void updateGameState(String message) {
-        //TODO: udpate GameState by parsing the message
+        //TODO: update GameState by parsing the message
     }
 }
