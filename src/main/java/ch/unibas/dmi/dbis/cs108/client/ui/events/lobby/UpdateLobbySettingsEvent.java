28db1811eb1ea183e9ef348e929bae399e9bb666
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:11:38 +0200

    fix(UpdateLobbySettingsEvent): improve event type identifier format and enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/UpdateLobbySettingsEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/UpdateLobbySettingsEvent.java
@@ -2,31 +2,57 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.lobby;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * UIEvent sent by the host to update a specific lobby setting.
+ */
 public class UpdateLobbySettingsEvent implements UIEvent {
+
     private final String lobbyId;
     private final String settingKey;
     private final String settingValue;
 
+    /**
+     * Constructs an UpdateLobbySettingsEvent.
+     *
+     * @param lobbyId      the ID of the lobby
+     * @param settingKey   the setting key (e.g., "maxPlayers")
+     * @param settingValue the setting value (e.g., "6")
+     */
     public UpdateLobbySettingsEvent(String lobbyId, String settingKey, String settingValue) {
         this.lobbyId = lobbyId;
         this.settingKey = settingKey;
         this.settingValue = settingValue;
     }
 
+    /**
+     * Returns the ID of the lobby.
+     *
+     * @return lobby ID
+     */
     public String getLobbyId() {
         return lobbyId;
     }
 
+    /**
+     * Returns the setting key.
+     *
+     * @return setting key
+     */
     public String getSettingKey() {
         return settingKey;
     }
 
+    /**
+     * Returns the setting value.
+     *
+     * @return setting value
+     */
     public String getSettingValue() {
         return settingValue;
     }
 
     @Override
     public String getType() {
-        return "UPDATELOBBYSETTINGS";
+        return "UPDATE_LOBBY_SETTINGS";
     }
 }
