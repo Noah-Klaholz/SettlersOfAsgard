bc27b2343fba0b56ce9b290f0809f1ce8daac1e5
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:08:27 +0200

    fix(PricesUIEvent): update event type identifier and enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/PricesUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/PricesUIEvent.java
@@ -2,14 +2,23 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * Event for requesting or updating game prices.
+ */
 public class PricesUIEvent implements UIEvent {
 
+    /**
+     * Constructs a PricesUIEvent.
+     */
     public PricesUIEvent() {
-        // No data needed for this event
+        // No specific data needed for this event currently.
     }
 
+    /**
+     * @return the event type identifier
+     */
     @Override
     public String getType() {
-        return "PRICES";
+        return "PRICES_REQUEST";
     }
 }
