55292f9950cb829d163098a8252315c318d8cc31
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:05:18 +0200

    refactor(ErrorEvent): improve code clarity by enhancing comments and fixing formatting

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/ErrorEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/ErrorEvent.java
@@ -5,7 +5,7 @@ import ch.unibas.dmi.dbis.cs108.client.networking.events.ErrorEvent.ErrorSeverit
 
 /**
  * UIEvent representing an error in the application.
- * This event is triggered when an error occurs, providing details about the error.
+ * Provides details about the error.
  */
 public class ErrorEvent implements UIEvent {
     private final Instant timestamp = Instant.now();
@@ -14,11 +14,11 @@ public class ErrorEvent implements UIEvent {
     private final ErrorSeverity severity;
 
     /**
-     * Constructor for ErrorEvent.
-     *.
-     * @param errorCode    The code representing the error.
-     * @param errorMessage A message describing the error.
-     * @param severity     The severity of the error.
+     * Constructs an ErrorEvent.
+     *
+     * @param errorCode    code representing the error
+     * @param errorMessage message describing the error
+     * @param severity     severity of the error
      */
     public ErrorEvent(String errorCode, String errorMessage, ErrorSeverity severity) {
         this.errorCode = errorCode;
@@ -27,36 +27,36 @@ public class ErrorEvent implements UIEvent {
     }
 
     /**
-     * Get the timestamp of the event.
+     * Returns the timestamp of the event.
      *
-     * @return The timestamp.
+     * @return timestamp
      */
     public Instant getTimestamp() {
         return timestamp;
     }
 
     /**
-     * Get the code representing the error.
+     * Returns the code representing the error.
      *
-     * @return The error code.
+     * @return error code
      */
     public String getErrorCode() {
         return errorCode;
     }
 
     /**
-     * Get the message describing the error.
+     * Returns the message describing the error.
      *
-     * @return The error message.
+     * @return error message
      */
     public String getErrorMessage() {
         return errorMessage;
     }
 
     /**
-     * Get the severity of the error.
+     * Returns the severity of the error.
      *
-     * @return The error severity.
+     * @return error severity
      */
     public ErrorSeverity getSeverity() {
         return severity;
