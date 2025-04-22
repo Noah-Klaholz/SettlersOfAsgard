c7abf695f5d302649095daaead0fce022118260f
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:09:05 +0200

    feat(UseStatueResponseEvent): implement event to represent response of UseStatue action

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/UseStatueResponseEvent.java
@@ -0,0 +1,75 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
+
+import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
+
+/**
+ * Represents the response/result of a UseStatue action.
+ */
+public class UseStatueResponseEvent implements UIEvent {
+
+    private final boolean success;
+    private final String message;
+    private final int x;
+    private final int y;
+    private final String statueId;
+
+    /**
+     * Constructs a UseStatueResponseEvent.
+     *
+     * @param success  whether the action was successful
+     * @param message  optional message for success/failure
+     * @param x        x-coordinate of the statue
+     * @param y        y-coordinate of the statue
+     * @param statueId the ID of the statue
+     */
+    public UseStatueResponseEvent(boolean success, String message, int x, int y, String statueId) {
+        this.success = success;
+        this.message = message;
+        this.x = x;
+        this.y = y;
+        this.statueId = statueId;
+    }
+
+    /**
+     * @return true if the action was successful, false otherwise
+     */
+    public boolean isSuccess() {
+        return success;
+    }
+
+    /**
+     * @return the message associated with the response
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @return the x-coordinate of the statue
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return the y-coordinate of the statue
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @return the ID of the statue
+     */
+    public String getStatueId() {
+        return statueId;
+    }
+
+    /**
+     * @return the event type identifier
+     */
+    @Override
+    public String getType() {
+        return "USE_STATUE_RESPONSE";
+    }
+}
