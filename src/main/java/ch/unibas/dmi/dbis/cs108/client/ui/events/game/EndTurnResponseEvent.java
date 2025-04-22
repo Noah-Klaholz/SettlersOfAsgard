307000faa0e075c5b3b8a6e17740ff53a8b8c65a
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:07:53 +0200

    feat(EndTurnResponseEvent): add event to indicate the end of a turn and the next player's turn

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/EndTurnResponseEvent.java
@@ -0,0 +1,35 @@
+package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
+
+import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
+
+/**
+ * Indicates the end of a turn and potentially the start of the next.
+ */
+public class EndTurnResponseEvent implements UIEvent {
+
+    private final String nextPlayerName;
+
+    /**
+     * Constructs an EndTurnResponseEvent.
+     *
+     * @param nextPlayerName the name of the player whose turn it is now
+     */
+    public EndTurnResponseEvent(String nextPlayerName) {
+        this.nextPlayerName = nextPlayerName;
+    }
+
+    /**
+     * @return the name of the next player
+     */
+    public String getNextPlayerName() {
+        return nextPlayerName;
+    }
+
+    /**
+     * @return the event type identifier
+     */
+    @Override
+    public String getType() {
+        return "END_TURN_RESPONSE";
+    }
+}
