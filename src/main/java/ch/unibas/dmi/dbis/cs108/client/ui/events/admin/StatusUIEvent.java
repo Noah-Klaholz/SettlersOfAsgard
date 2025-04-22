dff11416e601c1a38c4065d9532dd3379c986c65
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:06:49 +0200

    refactor(StatusUIEvent): enhance documentation for clarity and consistency

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/StatusUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/StatusUIEvent.java
@@ -2,12 +2,21 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.admin;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * UIEvent representing a status update in the admin UI.
+ */
 public class StatusUIEvent implements UIEvent {
 
+    /**
+     * Constructs a new StatusUIEvent.
+     */
     public StatusUIEvent() {
         // No data needed for this event
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getType() {
         return "STATUS";
