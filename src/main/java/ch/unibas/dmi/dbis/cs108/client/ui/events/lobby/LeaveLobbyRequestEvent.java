4eddfb3787bac476548ae32db639acfddc070b59
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:10:36 +0200

    fix(LeaveLobbyRequestEvent): update event type identifier format and enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/LeaveLobbyRequestEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/LeaveLobbyRequestEvent.java
@@ -2,19 +2,32 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.lobby;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * UIEvent representing a request to leave a lobby.
+ */
 public class LeaveLobbyRequestEvent implements UIEvent {
     private final String lobbyId;
 
+    /**
+     * Constructs a LeaveLobbyRequestEvent.
+     *
+     * @param lobbyId the ID of the lobby to leave
+     */
     public LeaveLobbyRequestEvent(String lobbyId) {
         this.lobbyId = lobbyId;
     }
 
+    /**
+     * Returns the ID of the lobby to leave.
+     *
+     * @return lobby ID
+     */
     public String getLobbyId() {
         return lobbyId;
     }
 
     @Override
     public String getType() {
-        return "LEAVELOBBYREQUEST";
+        return "LEAVE_LOBBY_REQUEST";
     }
 }
