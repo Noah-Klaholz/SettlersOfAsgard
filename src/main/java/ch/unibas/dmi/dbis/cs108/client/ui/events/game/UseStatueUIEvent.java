92fd2dc1ca9fb849dfa6b9b51dbe9c8610963fdf
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:09:09 +0200

    fix(UseStatueUIEvent): update event type identifier format and enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/UseStatueUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/UseStatueUIEvent.java
@@ -2,12 +2,23 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * Event representing a request to use a statue on the board.
+ */
 public class UseStatueUIEvent implements UIEvent {
     private final int x;
     private final int y;
     private final int statueId;
     private final String useType;
 
+    /**
+     * Constructs a UseStatueUIEvent.
+     *
+     * @param x        the x-coordinate
+     * @param y        the y-coordinate
+     * @param statueId the ID of the statue
+     * @param useType  the type of use
+     */
     public UseStatueUIEvent(int x, int y, int statueId, String useType) {
         this.x = x;
         this.y = y;
@@ -15,24 +26,39 @@ public class UseStatueUIEvent implements UIEvent {
         this.useType = useType;
     }
 
+    /**
+     * @return the x-coordinate
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * @return the y-coordinate
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * @return the statue ID
+     */
     public int getStatueId() {
         return statueId;
     }
 
+    /**
+     * @return the use type
+     */
     public String getUseType() {
         return useType;
     }
 
+    /**
+     * @return the event type identifier
+     */
     @Override
     public String getType() {
-        return "USESTATUE";
+        return "USE_STATUE";
     }
 }
