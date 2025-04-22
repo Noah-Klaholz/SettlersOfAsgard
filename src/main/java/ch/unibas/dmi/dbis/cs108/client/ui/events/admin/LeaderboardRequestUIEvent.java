932ccd3e575c67d6d16127307db671ecb1517397
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:05:53 +0200

    refactor(LeaderboardRequestUIEvent): remove unnecessary comments and improve code clarity

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/LeaderboardRequestUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/LeaderboardRequestUIEvent.java
@@ -4,14 +4,11 @@ import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
 /**
  * UIEvent triggered when a user requests to view the leaderboard.
- * This event is published by the UI and handled by the CommunicationMediator.
  */
 public class LeaderboardRequestUIEvent implements UIEvent {
-    
+
     /**
-     * Gets the type of this event.
-     *
-     * @return The event type as a string.
+     * {@inheritDoc}
      */
     @Override
     public String getType() {
