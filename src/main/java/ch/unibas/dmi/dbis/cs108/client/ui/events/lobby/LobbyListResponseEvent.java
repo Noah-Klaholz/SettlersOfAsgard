5d53c674afe14a3e046c0231ab6881f00d21a246
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:11:12 +0200

    fix(LobbyListResponseEvent): update lobbies type and improve event type identifier format

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/LobbyListResponseEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/LobbyListResponseEvent.java
@@ -1,23 +1,36 @@
 package ch.unibas.dmi.dbis.cs108.client.ui.events.lobby;
 
-import ch.unibas.dmi.dbis.cs108.client.ui.controllers.LobbyScreenController;
+import ch.unibas.dmi.dbis.cs108.client.ui.controllers.LobbyScreenController.GameLobby;
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
-
 import java.util.List;
 
+/**
+ * UIEvent carrying the list of available lobbies from the server.
+ */
 public class LobbyListResponseEvent implements UIEvent {
-    private final List<LobbyScreenController.GameLobby> lobbies;
 
-    public LobbyListResponseEvent(List<LobbyScreenController.GameLobby> lobbies) {
+    private final List<GameLobby> lobbies;
+
+    /**
+     * Constructs a LobbyListResponseEvent.
+     *
+     * @param lobbies the list of available lobbies
+     */
+    public LobbyListResponseEvent(List<GameLobby> lobbies) {
         this.lobbies = lobbies;
     }
 
-    public List<LobbyScreenController.GameLobby> getLobbies() {
+    /**
+     * Returns the list of available lobbies.
+     *
+     * @return list of lobbies
+     */
+    public List<GameLobby> getLobbies() {
         return lobbies;
     }
 
     @Override
     public String getType() {
-        return "LOBBYLISTRESPONSE";
+        return "LOBBY_LIST_RESPONSE";
     }
 }
