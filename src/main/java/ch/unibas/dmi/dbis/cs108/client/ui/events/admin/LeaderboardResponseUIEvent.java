fda3a0e2c81096bdf28b080941ac1c51aec686dd
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:05:57 +0200

    refactor(LeaderboardResponseUIEvent): improve code clarity by removing unnecessary comments and formatting Javadoc

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/LeaderboardResponseUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/LeaderboardResponseUIEvent.java
@@ -1,26 +1,25 @@
 package ch.unibas.dmi.dbis.cs108.client.ui.events.admin;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
-
 import java.util.List;
 import java.util.Map;
 
 /**
  * UIEvent triggered when leaderboard data is received from the server.
- * This event is published by the CommunicationMediator and handled by the UI.
  */
 public class LeaderboardResponseUIEvent implements UIEvent {
     private final List<Map<String, Object>> leaderboardEntries;
-    
+
     /**
-     * Creates a new LeaderboardResponseUIEvent with the given leaderboard entries.
+     * Constructs a new LeaderboardResponseUIEvent.
      *
-     * @param leaderboardEntries A list of maps containing player data (name, score, rank, etc.)
+     * @param leaderboardEntries A list of maps containing player data (name, score,
+     *                           rank, etc.)
      */
-public LeaderboardResponseUIEvent(List<Map<String, Object>> leaderboardEntries) {
+    public LeaderboardResponseUIEvent(List<Map<String, Object>> leaderboardEntries) {
         this.leaderboardEntries = leaderboardEntries;
     }
-    
+
     /**
      * Gets the leaderboard entries.
      *
@@ -29,11 +28,9 @@ public LeaderboardResponseUIEvent(List<Map<String, Object>> leaderboardEntries) 
     public List<Map<String, Object>> getLeaderboardEntries() {
         return leaderboardEntries;
     }
-    
+
     /**
-     * Gets the type of this event.
-     *
-     * @return The event type as a string.
+     * {@inheritDoc}
      */
     @Override
     public String getType() {
