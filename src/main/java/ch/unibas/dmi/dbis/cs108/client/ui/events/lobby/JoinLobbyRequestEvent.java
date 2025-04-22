e6f9f9582d0dc2894e488877c8eaa2ced7de193f
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:09:47 +0200

    fix(JoinLobbyRequestEvent): update event type identifier format and enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/JoinLobbyRequestEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/JoinLobbyRequestEvent.java
@@ -2,25 +2,44 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.lobby;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * UIEvent representing a request to join a lobby.
+ */
 public class JoinLobbyRequestEvent implements UIEvent {
     private final String lobbyId;
     private final String playerName;
 
+    /**
+     * Constructs a JoinLobbyRequestEvent.
+     *
+     * @param lobbyId    the ID of the lobby to join
+     * @param playerName the name of the player joining
+     */
     public JoinLobbyRequestEvent(String lobbyId, String playerName) {
         this.lobbyId = lobbyId;
         this.playerName = playerName;
     }
 
+    /**
+     * Returns the ID of the lobby to join.
+     *
+     * @return lobby ID
+     */
     public String getLobbyId() {
         return lobbyId;
     }
 
+    /**
+     * Returns the name of the player joining.
+     *
+     * @return player name
+     */
     public String getPlayerName() {
         return playerName;
     }
 
     @Override
     public String getType() {
-        return "JOINLOBBYREQUEST";
+        return "JOIN_LOBBY_REQUEST";
     }
 }
