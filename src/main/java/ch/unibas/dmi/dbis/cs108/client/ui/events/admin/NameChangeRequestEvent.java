7a058f026b62d12b067df6521313a998f227d42a
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:06:07 +0200

    refactor(NameChangeRequestEvent): implement UIEvent interface and enhance code clarity

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/NameChangeRequestEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/admin/NameChangeRequestEvent.java
@@ -1,13 +1,36 @@
 package ch.unibas.dmi.dbis.cs108.client.ui.events.admin;
 
-public class NameChangeRequestEvent {
+import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
+
+/**
+ * UIEvent representing a request to change the player's name.
+ */
+public class NameChangeRequestEvent implements UIEvent {
     private final String newName;
 
+    /**
+     * Constructs a new NameChangeRequestEvent.
+     *
+     * @param newName the desired new name
+     */
     public NameChangeRequestEvent(String newName) {
         this.newName = newName;
     }
 
+    /**
+     * Gets the new name.
+     *
+     * @return the new name
+     */
     public String getNewName() {
         return newName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getType() {
+        return "NAME_CHANGE_REQUEST";
     }
 }
