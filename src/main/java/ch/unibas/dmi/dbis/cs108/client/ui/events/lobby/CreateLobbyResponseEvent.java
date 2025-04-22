7ae479342078970fc7f477d6867161edc9f61e4f
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:09:33 +0200

    refactor(CreateLobbyResponseEvent): update constructor and remove timestamp, enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/CreateLobbyResponseEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/CreateLobbyResponseEvent.java
@@ -1,29 +1,47 @@
 package ch.unibas.dmi.dbis.cs108.client.ui.events.lobby;
 
-import ch.unibas.dmi.dbis.cs108.client.networking.events.LobbyEvent;
+import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
-import java.time.Instant;
+/**
+ * UIEvent representing the response to a create lobby request.
+ * Indicates the lobby was created and joined by the creator.
+ */
+public class CreateLobbyResponseEvent implements UIEvent {
 
-public class CreateLobbyResponseEvent {
-    private final Instant timestamp = Instant.now();
-    private final String playerName;
     private final String lobbyName;
+    private final String hostName;
 
-    public CreateLobbyResponseEvent( String playerName, String lobbyName) {
-        this.playerName = playerName;
+    /**
+     * Constructs a CreateLobbyResponseEvent.
+     *
+     * @param lobbyName the name of the created lobby
+     * @param hostName  the name of the host (creator)
+     */
+    public CreateLobbyResponseEvent(String lobbyName, String hostName) {
         this.lobbyName = lobbyName;
+        this.hostName = hostName;
     }
 
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-
+    /**
+     * Returns the name of the created lobby.
+     *
+     * @return lobby name
+     */
     public String getLobbyName() {
         return lobbyName;
     }
 
+    /**
+     * Returns the name of the host (creator).
+     *
+     * @return host name
+     */
+    public String getHostName() {
+        return hostName;
+    }
+
+    @Override
+    public String getType() {
+        return "CREATE_LOBBY_RESPONSE";
+    }
 }
