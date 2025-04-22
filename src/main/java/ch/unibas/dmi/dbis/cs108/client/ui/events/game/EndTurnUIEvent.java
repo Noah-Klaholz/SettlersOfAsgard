e73a8485bcded1b4bf0f262f1e9ec300abbfe177
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:07:57 +0200

    fix(EndTurnUIEvent): update event type string format to match convention and enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/EndTurnUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/EndTurnUIEvent.java
@@ -2,14 +2,23 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * Event representing a request to end the current player's turn.
+ */
 public class EndTurnUIEvent implements UIEvent {
 
+    /**
+     * Constructs an EndTurnUIEvent.
+     */
     public EndTurnUIEvent() {
         // No data needed for this event
     }
 
+    /**
+     * @return the event type identifier
+     */
     @Override
     public String getType() {
-        return "ENDTURN";
+        return "END_TURN";
     }
 }
