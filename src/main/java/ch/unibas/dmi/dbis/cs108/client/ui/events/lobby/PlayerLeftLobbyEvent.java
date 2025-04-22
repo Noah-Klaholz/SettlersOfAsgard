fc07853f34dd8dcbb227ef8de596a7a706898de9
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:11:28 +0200

    fix(PlayerLeftLobbyEvent): update constructor to include lobbyId and improve event type identifier format

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/PlayerLeftLobbyEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/lobby/PlayerLeftLobbyEvent.java
@@ -2,19 +2,45 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.lobby;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * UIEvent indicating that a player has left a lobby.
+ */
 public class PlayerLeftLobbyEvent implements UIEvent {
+
+    private final String lobbyId;
     private final String playerName;
 
-    public PlayerLeftLobbyEvent(String playerName) {
+    /**
+     * Constructs a PlayerLeftLobbyEvent.
+     *
+     * @param lobbyId    the ID of the lobby the player left
+     * @param playerName the name of the player who left
+     */
+    public PlayerLeftLobbyEvent(String lobbyId, String playerName) {
+        this.lobbyId = lobbyId;
         this.playerName = playerName;
     }
 
+    /**
+     * Returns the ID of the lobby the player left.
+     *
+     * @return lobby ID
+     */
+    public String getLobbyId() {
+        return lobbyId;
+    }
+
+    /**
+     * Returns the name of the player who left.
+     *
+     * @return player name
+     */
     public String getPlayerName() {
         return playerName;
     }
 
     @Override
     public String getType() {
-        return "PLAYERLEFTLOBBY";
+        return "PLAYER_LEFT_LOBBY";
     }
 }
