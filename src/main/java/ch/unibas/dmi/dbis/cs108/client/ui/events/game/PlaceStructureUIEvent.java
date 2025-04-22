43e356d393821b8f2a4487d1593c7f485047a122
Sebastian Lukas Müller <sebastianl.mueller@stud.unibas.ch>
2025-04-22 16:08:16 +0200

    fix(PlaceStructureUIEvent): update event type string format and enhance documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/PlaceStructureUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/PlaceStructureUIEvent.java
@@ -2,31 +2,53 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * Event representing a request to place a structure on the board.
+ */
 public class PlaceStructureUIEvent implements UIEvent {
     private final int x;
     private final int y;
     private final int structureId;
 
+    /**
+     * Constructs a PlaceStructureUIEvent.
+     *
+     * @param x           the x-coordinate
+     * @param y           the y-coordinate
+     * @param structureId the ID of the structure
+     */
     public PlaceStructureUIEvent(int x, int y, int structureId) {
         this.x = x;
         this.y = y;
         this.structureId = structureId;
     }
 
+    /**
+     * @return the x-coordinate
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * @return the y-coordinate
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * @return the structure ID
+     */
     public int getStructureId() {
         return structureId;
     }
 
+    /**
+     * @return the event type identifier
+     */
     @Override
     public String getType() {
-        return "PLACESTRUCTURE";
+        return "PLACE_STRUCTURE";
     }
 }
