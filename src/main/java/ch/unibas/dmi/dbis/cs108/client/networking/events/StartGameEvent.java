cca6b702a79b205fb4fdba2c263e010a6720b32d
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 15:18:46 +0200

    docs(events): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/events/StartGameEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/events/StartGameEvent.java
@@ -2,6 +2,10 @@ package ch.unibas.dmi.dbis.cs108.client.networking.events;
 
 import java.time.Instant;
 
+/**
+ * Represents an event sent from the server to the client when a game starts.
+ * This event is used to notify the client about the start of a game.
+ */
 public class StartGameEvent implements Event {
 
     /**
