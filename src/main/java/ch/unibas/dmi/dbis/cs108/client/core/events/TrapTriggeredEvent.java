7b56daff30a69e3a24cee15cb413411cf5aa4499
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 14:00:29 +0200

    docs(events): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/events/TrapTriggeredEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/events/TrapTriggeredEvent.java
@@ -3,25 +3,41 @@ package ch.unibas.dmi.dbis.cs108.client.core.events;
 import ch.unibas.dmi.dbis.cs108.shared.game.Player;
 import ch.unibas.dmi.dbis.cs108.shared.game.Tile;
 
+/**
+ * UIEvent representing the triggering of a trap by a player.
+ * This event is triggered when a player triggers a trap in the game.
+ */
 public class TrapTriggeredEvent implements GameEvent {
     private final Player triggeredPlayer;
     //todo: private final sth trap;
     private final Tile tile;
 
+    /**
+     * Constructor for TrapTriggeredEvent.
+     *
+     * @param triggeredPlayer The player who triggered the trap.
+     * @param tile           The tile where the trap was triggered.
+     */
     public TrapTriggeredEvent(Player triggeredPlayer, Tile tile) {
         this.triggeredPlayer = triggeredPlayer;
         //this.trap = trap;
         this.tile = tile;
     }
 
+    /**
+     * Get the player who triggered the trap.
+     *
+     * @return The player.
+     */
     public Player getTriggeredPlayer() {
         return triggeredPlayer;
     }
 
-//    public Trap getTrap() {
-//        return trap;
-//    }
-
+    /**
+     * Get the tile where the trap was triggered.
+     *
+     * @return The tile.
+     */
     public Tile getTile() {
         return tile;
     }
