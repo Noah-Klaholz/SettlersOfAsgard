f20ce57e81e2d6850de46c0b66aea09bbfcffc75
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 15:18:38 +0200

    docs(events): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/events/NotificationEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/events/NotificationEvent.java
@@ -2,19 +2,38 @@ package ch.unibas.dmi.dbis.cs108.client.networking.events;
 
 import java.time.Instant;
 
+/**
+ * Represents a notification event in the game.
+ * This class encapsulates the details of a notification, including the message.
+ */
 public class NotificationEvent implements Event {
     private final Instant timestamp = Instant.now();
     private final String message;
 
+    /**
+     * Constructor for NotificationEvent.
+     *
+     * @param message The message associated with the notification event.
+     */
     public NotificationEvent(String message) {
         this.message = message;
     }
 
+    /**
+     * Gets the timestamp of the event.
+     *
+     * @return The timestamp.
+     */
     @Override
     public Instant getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Gets the message associated with the notification event.
+     *
+     * @return The message.
+     */
     public String getMessage() {
         return message;
     }
