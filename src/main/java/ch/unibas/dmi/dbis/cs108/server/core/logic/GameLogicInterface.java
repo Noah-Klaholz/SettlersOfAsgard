27b520e4c71b744b2c7bcfc596fb8a0c2d4f6133
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 15:48:04 +0200

    docs(logic): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/server/core/logic/GameLogicInterface.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/server/core/logic/GameLogicInterface.java
@@ -2,8 +2,16 @@ package ch.unibas.dmi.dbis.cs108.server.core.logic;
 
 import ch.unibas.dmi.dbis.cs108.server.core.structures.Command;
 
+/**
+ * This interface defines the interactions between Lobby and GameLogic.
+ * It provides methods to start the game and process commands.
+ */
 public interface GameLogicInterface {
     void startGame(String[] players);
 
+    /**
+     * Processes a command from a player.
+     * @param command The command to process.
+     */
     void processCommand(Command command);
 }
