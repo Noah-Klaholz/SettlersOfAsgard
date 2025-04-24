8b3693e1904330f6c68a022a8e290f7a7e5cef4b
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 15:36:37 +0200

    docs(game): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/BuyStatueUIEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/ui/events/game/BuyStatueUIEvent.java
@@ -2,17 +2,33 @@ package ch.unibas.dmi.dbis.cs108.client.ui.events.game;
 
 import ch.unibas.dmi.dbis.cs108.client.ui.events.UIEvent;
 
+/**
+ * UIEvent representing a request to buy a statue in the game.
+ */
 public class BuyStatueUIEvent implements UIEvent {
     private final int statueId;
 
+    /**
+     * Constructs a new BuyStatueUIEvent.
+     *
+     * @param statueId the ID of the statue to be bought
+     */
     public BuyStatueUIEvent(int statueId) {
         this.statueId = statueId;
     }
 
+    /**
+     * Gets the ID of the statue to be bought.
+     *
+     * @return the statue ID
+     */
     public int getStatueId() {
         return statueId;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getType() {
         return "BUY_STATUE";
