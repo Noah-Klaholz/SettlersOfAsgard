c2bf980d62e0f879ebf64ca81a2f91ed01a6a8a0
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 15:48:14 +0200

    docs(behaviors): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/shared/entities/Behaviors/Parameter.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/shared/entities/Behaviors/Parameter.java
@@ -1,19 +1,35 @@
 // Java
 package ch.unibas.dmi.dbis.cs108.shared.entities.Behaviors;
 
+/**
+ * The Parameter class represents a parameter with a name and a value.
+ * It is used to define various parameters in the game.
+ */
 public class Parameter {
     private String name;
     private double value;
 
+    /**
+     * Constructor to create a Parameter object.
+     *
+     * @param name  The name of the parameter.
+     * @param value The value of the parameter.
+     */
     public Parameter(String name, double value) {
         this.name = name;
         this.value = value;
     }
 
+    /**
+     * getName method to retrieve the name of the parameter.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * getValue method to retrieve the value of the parameter.
+     */
     public double getValue() {
         return value;
     }
