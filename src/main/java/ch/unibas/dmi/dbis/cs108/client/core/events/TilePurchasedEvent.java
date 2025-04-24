093790981cea9a0fad84fb1cc6976fbdf357179c
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 14:00:20 +0200

    docs(events): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/core/events/TilePurchasedEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/core/events/TilePurchasedEvent.java
@@ -3,19 +3,39 @@ package ch.unibas.dmi.dbis.cs108.client.core.events;
 import ch.unibas.dmi.dbis.cs108.shared.game.Player;
 import ch.unibas.dmi.dbis.cs108.shared.game.Tile;
 
+/**
+ * UIEvent representing the purchase of a tile by a player.
+ * This event is triggered when a player purchases a tile in the game.
+ */
 public class TilePurchasedEvent implements GameEvent {
     private final Player player;
     private final Tile tile;
 
+    /**
+     * Constructor for TilePurchasedEvent.
+     *
+     * @param player The player who purchased the tile.
+     * @param tile   The tile that was purchased.
+     */
     public TilePurchasedEvent(Player player, Tile tile) {
         this.player = player;
         this.tile = tile;
     }
 
+    /**
+     * Get the player who purchased the tile.
+     *
+     * @return The player.
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Get the tile that was purchased.
+     *
+     * @return The tile.
+     */
     public Tile getTile() {
         return tile;
     }
