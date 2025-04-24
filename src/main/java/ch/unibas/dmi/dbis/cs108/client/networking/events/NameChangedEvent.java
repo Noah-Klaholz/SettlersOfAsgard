dcac543ae19922e815151edb7ec78f3555e775c6
Jennifer Monique Staisch <j.staisch@stud.unibas.ch>
2025-04-24 15:18:32 +0200

    docs(events): add documentation

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/events/NameChangedEvent.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/networking/events/NameChangedEvent.java
@@ -2,19 +2,38 @@ package ch.unibas.dmi.dbis.cs108.client.networking.events;
 
 import java.time.Instant;
 
+/**
+ * Represents an event that occurs when a player's name changes in the game.
+ * This class encapsulates the details of the name change event, including the new name.
+ */
 public class NameChangedEvent implements Event {
     private final Instant timestamp = Instant.now();
     private final String newName;
 
+    /**
+     * Constructor for NameChangedEvent.
+     *
+     * @param newName The new name of the player.
+     */
     public NameChangedEvent(String newName) {
         this.newName = newName;
     }
 
+    /**
+     * Getter for the timestamp of the event.
+     *
+     * @return The timestamp of the event.
+     */
     @Override
     public Instant getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Getter for the new name of the player.
+     *
+     * @return The new name of the player.
+     */
     public String getNewName() {
         return newName;
     }
